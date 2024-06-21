a9f2c02b1c4f390664c17a512773eec82d533c77
Kuldeep Singh <rajputsinghkuldeep@gmail.com>
2024-06-21 11:06:17 +0530

    LeetCode Rank Of Number

---
+++ LeetCode/LeetCode_1331_Rank_Of_Number.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class LeetCode_1331_Rank_Of_Number {
+
+    
+    public static void main(String s[]){
+        Scanner scan = new Scanner(System.in);
+
+
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        
+        Set<Integer> set = new TreeSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int rank = 1;
+        for (int num : set) {
+            map.put(num, rank++);
+        }
+        System.out.println(map);
+
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = map.get(arr[i]);
+        }
+
+        System.out.println("Rank: ");
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+
+    }
+}
